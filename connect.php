95c09f9135e8e6200ce51d0fd450f876d30d2764
syahiramr <52453883+syahiraMR@users.noreply.github.com>
2019-09-16 02:22:57 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,5 +2,5 @@
 $servername = "localhost";    //local host
 $username = "root";          //name of phpmyadmin
 $password = "123456say"; 	 //password of phpmyadmin
-$dbname = "u773280762_icas"; //database name
+$dbname = "efedb"; //database name
 ?>
