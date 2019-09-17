be5d9ad5b82531486dc25118280a1ccab16efd54
syahiramr <52453883+syahiraMR@users.noreply.github.com>
2019-09-17 21:57:36 +0800

    code update

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "123456say";
-$dbname = "efedb";
+$servername = 'localhost';
+$username = 'root';
+$password = '123456say';
+$dbname = 'efedb';
 ?>
