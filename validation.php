98b7471c39ad5389496ae6a4954f9b70b8bd9324
syahiramr <52453883+syahiraMR@users.noreply.github.com>
2019-10-02 03:33:19 +0800

    //

--- validation.php
+++ validation.php
@@ -34,7 +34,7 @@
 			       
 			        if($role == 1)
 			        {
-			            header("Location: http://www.efequizonline.tk/page5.php");
+			            header("Location: http://www.efequizonline.tk/page5.php/");
 	                    exit;
 			        }
 			        else if($role == 2)
