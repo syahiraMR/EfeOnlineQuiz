cc4ff27d0cf7c14b7640dc8c9b2c99b98f2898e6
syahiramr <52453883+syahiraMR@users.noreply.github.com>
2019-10-02 01:31:13 +0800

    change ""

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
-$servername = '127.0.0.1:3307';
-$username = 'root';
-$password = '123456say';
-$dbname = 'efedb';
+$servername = "127.0.0.1:3307";
+$username = "root";
+$password = "123456say";
+$dbname = "efedb";
 ?>
